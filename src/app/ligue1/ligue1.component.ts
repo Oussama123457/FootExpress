eb96a03d50ae8d9f46dc6d12d09e867d3ef81b85
Oussama123457 <boudallaa.oussama@gmail.com>
2024-02-01 15:43:01 +0100

    hands-on angular and bootstrap

---
+++ src/app/ligue1/ligue1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ligue1',
+  standalone: true,
+  imports: [],
+  templateUrl: './ligue1.component.html',
+  styleUrl: './ligue1.component.scss'
+})
+export class Ligue1Component {
+
+}
